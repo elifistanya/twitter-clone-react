e7871b59ab9eee834a98059e928c11815647c5cc
Elif Türk <elif223789@gmail.com>
2022-01-08 02:27:26 +0300

    fix

---
+++ with-tailwindcss-app/atoms/modalAtom.js
@@ -0,0 +1,11 @@
+﻿import { atom } from "recoil";
+
+export const modalState = atom({
+    key: "modalState",
+    default: false,
+});
+
+export const postIdState = atom({
+    key: "postIdState",
+    default: "",
+});
